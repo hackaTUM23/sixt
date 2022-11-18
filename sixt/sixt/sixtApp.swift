b5aaf336efbe77b899520a82d4f252f498aea58d
Nikolai Madlener <nikolai.madlener@outlook.de>
2022-11-18 23:52:25 +0100

    app init

---
+++ sixt/sixt/sixtApp.swift
@@ -0,0 +1,17 @@
+//
+//  sixtApp.swift
+//  sixt
+//
+//  Created by Nikolai Madlener on 18.11.22.
+//
+
+import SwiftUI
+
+@main
+struct sixtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
