b81684330d9158b5776d0ae07afac7b0afa1f52e
Martin Fink <martin@finkmartin.com>
2022-11-19 15:57:15 +0100

    Added CarDetailView to the components

---
+++ sixt/sixt/Views/Components/CarDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  CarDetailView.swift
+//  sixt
+//
+//  Created by Leon Friedmann on 19.11.22.
+//
+
+import SwiftUI
+
+struct CarDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CarDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarDetailView()
+    }
+}
